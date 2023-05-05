8c0bad8a456d01f1036a24df5536772d243bcd0f
tuankthai <ttlive4jc@gmail.com>
2023-05-04 18:59:21 -0700

    finished easy exercises

--- easy.js
+++ easy.js
@@ -7,12 +7,18 @@ function findLargestNumber(numbers) {
 
 const numberArr = [5, 1, 9, 3, 7];
 
-console.log(findLargestNumber(numberArr)); // Output: 9
+// console.log(findLargestNumber(numberArr)); // Output: 9
 
 
 // Write a function that takes in a string and returns the number of vowels in the string.
 function countVowels(str) {
   // Your code here
+  let vowelCnt = 0
+  const vowels = ["a", "e", "i", "o", "u",]
+  for (letter of str) {
+    if (vowels.includes(letter) ) vowelCnt++
+  }
+  return vowelCnt
 }
 
 const str1 = 'Hello world';
@@ -23,6 +29,8 @@ console.log(countVowels(str1)); // Output: 3
 // Write a function that takes in an object and a key, and returns the value of that key in the object.
 function getValueByKey(obj, key) {
   // Your code here
+  
+  return obj[key]
 }
 
 const person = {
@@ -32,4 +40,6 @@ const person = {
   };
   
 console.log(getValueByKey(person, 'age')); // Output: 32
+console.log(getValueByKey(person, 'name')); // Output: 32
+console.log(getValueByKey(person, 'job')); // Output: 32
   
